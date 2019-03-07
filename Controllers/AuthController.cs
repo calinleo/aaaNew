40b8004673fba1e50fe0d6b8857d47367f6dc637
Calin Leordean <CLeordean@grt.granitenet.com>
2019-03-07 13:37:56 -0500

    Created the Register method

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using aaaNew.Data;
+using aaaNew.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aaaNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult>Register(string username, string password)
+        {
+            // validate request
+            username = username.ToLower();
+            if (await _repo.UserExists(username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User    
+            {
+                Username = username
+            };
+            var crreatedUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
