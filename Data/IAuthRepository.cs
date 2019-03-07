45f5c1c7259617134d5f9cb62b35c208f732e99b
Calin Leordean <CLeordean@grt.granitenet.com>
2019-03-07 11:33:11 -0500

    Repository , Hash and Salt

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using aaaNew.Models;
+
+namespace aaaNew.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
