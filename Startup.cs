c5846d39eb72ce9b9dd395046b09ada2aafb49dd
Calin Leordean <CLeordean@grt.granitenet.com>
2019-03-14 07:27:46 -0400

    Seeding the database from local Json

--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,7 @@ namespace aaaNew
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
@@ -52,7 +53,7 @@ namespace aaaNew
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -77,6 +78,7 @@ namespace aaaNew
             }
 
             //app.UseHttpsRedirection();
+            // seeder.SeedUsers();   // used to seed the DB
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
