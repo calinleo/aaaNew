75538dddf32095d62d7ab95ee20c61b5188b2f06
Calin Leordean <CLeordean@grt.granitenet.com>
2019-03-21 14:59:29 -0400

    Cloudinary API for Photo Storage

--- Models/Photo.cs
+++ Models/Photo.cs
@@ -9,6 +9,7 @@ namespace aaaNew.Models
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
+        public string PublicId { get; set; } // for Cloudinary API
         public User User { get; set; }
         public int UserId { get; set; }
     }
